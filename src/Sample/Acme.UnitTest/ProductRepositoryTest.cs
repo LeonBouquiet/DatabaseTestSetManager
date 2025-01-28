250da1caf53b68b7683ae9830ddd9d1bc2dbf127
Léon Bouquiet <Leon.Bouquiet@infosupport.com>
2025-01-28 13:35:03 +0100

    Added Unittest project, currently without testset.

---
+++ src/Sample/Acme.UnitTest/ProductRepositoryTest.cs
@@ -0,0 +1,38 @@
+﻿using DatabaseTestSetManager;
+
+namespace Acme.UnitTest;
+
+[TestClass]
+public class ProductRepositoryTest: DatabaseTestBase<AcmeDbContext>
+{
+	public override string ConnectionString => "Data Source=(LocalDB)\\MSSQLLocalDB;Initial Catalog=AcmeDB;Integrated Security=True;Trust Server Certificate=True";
+
+	/// <summary>
+	/// Defines the Default test set.
+	/// </summary>
+	public override void OnDefineTestSets()
+	{
+		TestSetManager.DefineTestSet("Default", setup =>
+			 setup.FromAllEmbeddedSqlScripts(inAssemblyThatDefines: this));
+	}
+
+	private ProductRepository CreateProductRepository()
+	{
+		return new ProductRepository(base.DbContext);
+	}
+
+	[TestMethod]
+	public async Task GetAllProducts_ReturnsData()
+	{
+		//Arrange
+		ProductRepository repository = CreateProductRepository();
+
+		//Act
+		List<Product> products = await repository.GetAllProducts();
+
+		//Assert
+		Assert.IsNotNull(products);
+		Assert.IsTrue(products.Any());
+	}
+
+}
